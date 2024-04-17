61dd06975bb64a1d63d5927498cddd2fba14f65a
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-17 09:39:53 +0200

    ajout des premier fichier de code et de base de donnée

---
+++ ConsoleApp1/Calculus.cs
@@ -0,0 +1,8 @@
+﻿public class Calculus
+{
+    public int Id { get; set; }
+
+    public int Value1 { get; set; }
+    public string Operateur { get; set;}
+    public int Value2 { get; set; }
+}
---
+++ ConsoleApp1/Migrations/20240416153715_addValue.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class addValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calculus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Value1 = table.Column<int>(type: "INTEGER", nullable: false),
+                    Operateur = table.Column<string>(type: "TEXT", nullable: false),
+                    Value2 = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calculus", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calculus");
+        }
+    }
+}

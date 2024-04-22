c94b0059770b476f5a42385da21b933464444d20
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-22 10:56:53 +0200

    Ajout de la classe division

---
+++ Calculator/Div.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Div : IOperation
+    {
+        public Div(float number1, float number2)
+        {
+            this.Number1 = number1;
+            this.Number2 = number2;
+
+        }
+
+        public float Number1
+        {
+            get; set;
+        }
+
+        public float Number2
+        {
+            get; set;
+        }
+
+        public float calc()
+        {
+            return Number1 / Number2;
+        }
+
+    }
+}

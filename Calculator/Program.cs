fb4d37cd8e87aebe2d3a7e83d6af2bc8f19df538
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-22 10:54:33 +0200

    Ajout de la saisie utilisateur

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿using Calculator;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Vous devez coder une calculatrice,sans GUI d'abord mais avec une database. Et ensuite yes avec GUI");
+        Console.Write("Nombre 1 : ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Operateur (+ , - , * , /): ");
+        string calc = Console.ReadLine();
+
+        Console.Write("Nombre 2 : ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        IOperation operation = null;
+        switch (calc)
+        {
+            case "+":
+                operation = new Add(number1, number2);
+                break;
+            case "-":
+                operation = new Sub(number1, number2);
+                break;
+            case "*":
+                operation = new Mult(number1, number2);
+                break;
+            case "/":
+                operation = new Div(number1, number2);
+                break;
+            default:
+                Console.WriteLine("Error!");
+                break;
+        }
+        Console.WriteLine("Le résultat de {0} {1} {2} est : {3}", number1, calc, number2, operation.calc());
+
+
     }
 }

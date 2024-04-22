28fa7e6986409ae0253f7a01efa854ceeba697ee
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-22 16:37:23 +0200

    design interface calculatrice

--- Interface/MainWindow.xaml.cs
+++ Interface/MainWindow.xaml.cs
@@ -21,5 +21,23 @@ namespace Interface
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

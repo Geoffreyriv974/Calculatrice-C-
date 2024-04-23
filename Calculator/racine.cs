c718fdbe78288815e0d1a8707dd2795abb1fec49
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-23 14:37:51 +0200

    Ajout de la class pour la racine carrée

---
+++ Calculator/racine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Racine : IOperation
+    {
+        public Racine(float number1)
+        {
+            this.Number1 = number1;
+        }
+
+        public float Number1
+        {
+            get; set;
+        }
+
+        public float Number2
+        {
+            get; set;
+        }
+
+        public float calc()
+        {
+            float result = (float)Math.Sqrt(Number1);
+            return result;
+        }
+
+    }
+}

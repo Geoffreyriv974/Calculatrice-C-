35aab83ffce82b1c57f46faef98914da4a92e85e
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-04-23 12:07:07 +0200

    Ajout de la class puisssance carré

---
+++ Calculator/carre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Carre : IOperation
+    {
+        public Carre(float number1)
+        {
+            this.Number1 = number1;
+        }
+
+        public float Number1
+        {
+            get; set;
+        }
+
+        public float calc()
+        {
+            return Number1 * Number1;
+        }
+
+    }
+}
